12e96fe1f764a2581a481f88f699ef81f4c89990
imRajAryan09 <raj.12020162@lpu.in>
2022-01-24 01:29:47 +0530

    css file fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,38 @@
 import React, { useState } from "react";
-import "./mudit.css";
+import "./App.css";
 
 function App() {
-  const [rem, setRem] = useState(1);
-  const [px, setpx] = useState(16);
-  return (
-    <div className="app">
-      <div className="header">
-        <h1>PX to REM Converter</h1>
-      </div>
-      <div className="conversion">
-        <div className="value">
-          <form action="" className="form">
-            <input
-              type="text"
-              name="px"
-              value={px}
-              format="technical"
-              onChange={(e) => {
-                setpx(e.target.value);
-                setRem((e.target.value / 16).toFixed(3));
-              }}
-            />
-            <span>px</span>
-          </form>
-        </div>
-        <div className="answer">
-          <p>{rem} <span>rem</span></p>
-        </div>
-      </div>
-    </div>
-  );
+	const [rem, setRem] = useState(1);
+	const [px, setpx] = useState(16);
+	return (
+		<div className="app">
+			<div className="header">
+				<h1>PX to REM Converter</h1>
+			</div>
+			<div className="conversion">
+				<div className="value">
+					<form action="" className="form">
+						<input
+							type="text"
+							name="px"
+							value={px}
+							format="technical"
+							onChange={(e) => {
+								setpx(e.target.value);
+								setRem((e.target.value / 16).toFixed(3));
+							}}
+						/>
+						<span>px</span>
+					</form>
+				</div>
+				<div className="answer">
+					<p>
+						{rem} <span>rem</span>
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
