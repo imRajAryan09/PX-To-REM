5590d862b48846170ca1b58d8c2a875d9b6360dc
imRajAryan09 <raj.12020162@lpu.in>
2022-01-23 14:19:46 +0530

    logic implemented

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+	const [pixel, setPixel] = useState(0);
+	const [rem, setRem] = useState(0);
+	return (
+		<div className="app">
+			<div className="header">
+				<h1>PX To REM Converter</h1>
+			</div>
+			<div className="conversion">
+				<div className="value">
+					<form action="" className="form">
+						<input
+							type="text"
+							name="px"
+							placeholder="PX"
+							onChange={(e) => {
+								setPixel(e.target.value);
+								setRem(e.target.value / 16);
+							}}
+						/>
+						{console.log(pixel, rem)}
+					</form>
+				</div>
+				<div className="answer">
+					<p>{rem} REM</p>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
